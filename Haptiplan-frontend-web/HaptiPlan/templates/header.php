b1cdb0aed8b95bbef862e8a19c5730a46d9859ae
ninamerkt <146719778+ninamerkt@users.noreply.github.com>
2023-12-11 12:42:19 +0100

    Update header.php

--- Haptiplan-frontend-web/HaptiPlan/templates/header.php
+++ Haptiplan-frontend-web/HaptiPlan/templates/header.php
@@ -1,24 +1,13 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" aria-current="page" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/">Unternehmsplanspiel</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link"  href="http://localhost/Haptiplan-frontend-web/HaptiPlan/templates/Beschaffung">Beschaffung</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/Produktion">Produktion</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/Auftraege">Aufträge</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/templates/create_machine.php">Maschine</a>
-        </li>
-      </ul>
+<link rel="stylesheet" href="../styles/header.css">
+
+<nav class="navbar">
+  <div class="inner-navbar">
+    <a class="active" aria-current="page" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/">Unternehmsplanspiel</a>
+    <ul class="element-list">
+      <a class="element"  href="http://localhost/Haptiplan-frontend-web/HaptiPlan/templates/Beschaffung">Beschaffung</a>
+      <a class="element" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/Produktion">Produktion</a>
+      <a class="element" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/Auftraege">Aufträge</a>
+      <a class="element" href="http://localhost/Haptiplan-frontend-web/HaptiPlan/templates/create_machine.php">Maschine</a>
+    </ul>
     </div>
-  </div>
 </nav>
