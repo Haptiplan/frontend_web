522d6bdb9af6ad30792c98f5f559f0b53da8efbd
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2023-12-12 10:26:42 +0100

    changed procurement_machine and create machine

--- Haptiplan-frontend-web/HaptiPlan/scripts/procurement_machine.js
+++ Haptiplan-frontend-web/HaptiPlan/scripts/procurement_machine.js
@@ -1,30 +1,42 @@
+function getMachine() {
+  fetch('http://localhost/haptiplan-backend/HaptiPlan/machine')
+  .then(response => response.json())
+  .then(data => {
 
-/*fetch("http://localhost/Haptiplan-backend/HaptiPlan/machine")
-.then(respone => respone.json())
-.then(function(machines){
-    var machineList = document.getElementById("machineList");
- 
-    machines.forEach(function (machine) {
-        var machineItem = document.createElement("li");
-        machineItem.innerHTML = "Name: " + machine.name;
-        machineList.appendChild(machineItem);
-    });
-})*/
+      let dataList = document.getElementById('dataList');
+      // Clear any existing items in the list
+      dataList.innerHTML = '';
 
-fetch("http://localhost/Haptiplan-backend/HaptiPlan/machine")
-  .then((response) => {
-    return response.json();
+      // Loop through the data and create list items
+      data.forEach(item => {
+          const listItem = document.createElement('li');
+          listItem.textContent = item.description;
+          dataList.appendChild(listItem);
+      });
   })
-  .then((machines) => {
-    console.log("Daten erfolgreich abgerufen:", machines);
-    const machineList = document.getElementById("machines");
-    machines.forEach((machine) => {
-      const tmpl = document.getElementById("machine-template").content.cloneNode(true);
-      tmpl.querySelector(".machineName").innerText = machine.machine_name;
-      tmpl.querySelector(".machineID").innerText = machine.id;
-      machineList.appendChild(tmpl);
-    });
+}
+
+getMachine()
+
+
+document.getElementById('dataForm').addEventListener('submit', submitForm);
+
+function submitForm(event) {
+
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  fetch('http://localhost/haptiplan-backend/HaptiPlan/machine', {
+      method: 'POST',
+      body: formData
   })
+  .then(response => { return response.json(); })
+  .then(data => {
+      console.log('Data successfully inserted');
+      getMachine();
+  })
+}
   
 
 
--- Haptiplan-frontend-web/HaptiPlan/templates/create_machine.php
+++ Haptiplan-frontend-web/HaptiPlan/templates/create_machine.php
@@ -12,18 +12,14 @@
 <?php include_once('header.php'); ?>
     <h1>Maschinen</h1>
     
-    <form action="/Haptiplan-backend/HaptiPlan/machine" method="post">
-        <label for="machine_name">Name</label>
-        <input type="text" name="machine_name">
-        <input type="submit" name="submit" value ="senden">
-    </form> 
-    <template id="machine-template">
-        <div class="machineList">
-            <div class="machineID"></div>
-            <div class="machineName"></div>
+    <form id="dataForm">
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description" required>
         </div>
-    </template>
-    <div id="machines"></div>
+        <button type="submit">create machine</button>
+    </form>
+    <ul id='dataList'></ul>
     <script src="../scripts/procurement_machine.js"></script>
 </body>
 </html>
