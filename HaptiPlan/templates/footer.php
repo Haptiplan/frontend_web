fb12bc9c9a46fdc6ce748226b84a921dae2a39c9
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2023-12-18 09:41:48 +0100

    implemented header and footer

--- HaptiPlan/templates/footer.php
+++ HaptiPlan/templates/footer.php
@@ -1,10 +1,14 @@
+<link rel="stylesheet" href="../styles/footer.css">
+
 <!-- Footer -->
-<footer class="footer text-center text-lg-start bg-light text-muted">
+<footer>
+
   <!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+  <div class="" >
     © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Jade HS</a>
+    <a class="" >Jade HS</a>
   </div>
   <!-- Copyright -->
+
 </footer>
 <!-- Footer -->
