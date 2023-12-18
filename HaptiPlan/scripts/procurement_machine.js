fdf380d0bd7362db3e6c9e93ba2d22dc82ded6a6
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2023-12-18 08:48:14 +0100

    machines are templates now

--- HaptiPlan/scripts/procurement_machine.js
+++ HaptiPlan/scripts/procurement_machine.js
@@ -1,19 +1,21 @@
 function getMachine() {
   fetch('http://localhost/haptiplan-backend/HaptiPlan/machine')
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
 
       let dataList = document.getElementById('dataList');
       // Clear any existing items in the list
       dataList.innerHTML = '';
 
+      const machineTemplate = document.getElementById('machineTemplate');
+
       // Loop through the data and create list items
       data.forEach(item => {
-          const listItem = document.createElement('li');
-          listItem.textContent = item.description;
-          dataList.appendChild(listItem);
-      });
-  })
+        const machineInstance = machineTemplate.content.cloneNode(true);
+        machineInstance.querySelector('.machine_name').textContent = item.description;
+        dataList.appendChild(machineInstance);
+    });
+})
 }
 
 getMachine()
@@ -28,17 +30,19 @@ function submitForm(event) {
   var description_value = formData.get('description');
 
   fetch('http://localhost/haptiplan-backend/HaptiPlan/machine', {
-      method: 'POST',
-      body: JSON.stringify({"description": description_value})
+    method: 'POST',
+    body: JSON.stringify({ "description": description_value })
   })
-  .then(response => { return response.json(); })
-  .then(data => {
+    .then(response => {
+       return response.json(); 
+      })
+    .then(data => {
       console.log('Data successfully inserted');
       getMachine();
-  })
+    })
 
 }
-  
+
 
 
 
