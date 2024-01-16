eaa7a38477f4277f30b7f88dba9f834b5ccc3855
Omar kanoune <38401772+omarKanoune@users.noreply.github.com>
2024-01-16 15:12:40 +0100

    Code refactoring  and Issues gefixet

--- HaptiPlan/scripts/procurement_machine.js
+++ HaptiPlan/scripts/procurement_machine.js
@@ -12,7 +12,7 @@ function getMachine() {
       // Loop through the data and create list items
       data.forEach(item => {
         const machineInstance = machineTemplate.content.cloneNode(true);
-        machineInstance.querySelector('.machine_name').textContent = item.description;
+        machineInstance.querySelector('.machine_name').textContent = item.machine_name;
         dataList.appendChild(machineInstance);
     });
 })
@@ -21,27 +21,40 @@ function getMachine() {
 getMachine()
 
 
-document.getElementById('dataForm').addEventListener('submit', submitForm);
+const form = document.querySelector('form');
 
-function submitForm(event) {
-  event.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  const formData = new FormData(event.target);
-  var description_value = formData.get('description');
+const formData = new FormData(form);
+const machine_name = formData.get('machine_name');
+const machine_capacity= parseInt(formData.get('machine_capacity'));
+const machine_price= parseFloat(formData.get('machine_price'));
+const machine_duration = parseInt(formData.get('machine_duration'));
+const machine_period = parseInt(formData.get('machine_period'));
 
   fetch('http://localhost/haptiplan-backend/HaptiPlan/machine', {
-    method: 'POST',
-    body: JSON.stringify({ "description": description_value })
-  })
-    .then(response => {
-       return response.json(); 
-      })
-    .then(data => {
-      console.log('Data successfully inserted');
-      getMachine();
+    method: "POST",
+    body: JSON.stringify({
+        "machine_name": machine_name,
+        "machine_capacity":machine_capacity,
+        "machine_price": machine_price,
+        "machine_duration": machine_duration,
+        "machine_period": machine_period
     })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    getMachine();
 
-}
+  })
+  .catch(error => console.log(error))
+
+})
+
+
+
 
 
 
