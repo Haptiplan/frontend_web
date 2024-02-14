2a16a84f1c0b75d2b75639273d1cd5acdbca0a34
HichamElBadr <141155879+HichamElBadr@users.noreply.github.com>
2024-02-14 10:02:44 +0100

    implemented get and add credit

--- HaptiPlan/scripts/procurement_credit.js
+++ HaptiPlan/scripts/procurement_credit.js
@@ -1,28 +1,28 @@
 function getCredit() {
-    fetch(window.location.origin + '/backend/HaptiPlan/credit')
-      .then(response => response.json())
-      .then(data => {
-  
-        let dataList = document.querySelector('.dataList');
-        // Clear any existing items in the list
-        dataList.innerHTML = '';
-  
-        const creditTemplate = document.querySelector('.creditTemplate');
-  
-        // Loop through the data and create list items
-        data.forEach(item => {
-          const creditInstance = creditTemplate.content.cloneNode(true);
-  
-          creditInstance.querySelector('.credit_amount').textContent = item.credit_amount;
-          dataList.appendChild(creditInstance);
-        });
+  fetch(window.location.origin + '/backend/haptiplan/credit')
+    .then(response => response.json())
+    .then(data => {
+
+      let dataList = document.querySelector('.dataList');
+      // Clear any existing items in the list
+      dataList.innerHTML = '';
+
+      const creditTemplate = document.querySelector('.creditTemplate');
+
+      // Loop through the data and create list items
+      data.forEach(item => {
+        const creditInstance = creditTemplate.content.cloneNode(true);
+
+        creditInstance.querySelector('.credit_amount').textContent = item.credit_amount;
+        dataList.appendChild(creditInstance);
       });
-  }
-  
-  getCredit();
+    });
+}
+
+getCredit();
 
 //Add Credit
-/** 
+
 const form = document.querySelector('.add_form');
 
 form.addEventListener('submit', (e) => {
@@ -31,16 +31,15 @@ form.addEventListener('submit', (e) => {
 const formData = new FormData(form);
 const credit_amount = formData.get('credit');
 
-
-  fetch(window.location.origin + '/backend/HaptiPlan/credit', {
+  fetch(window.location.origin + '/backend/haptiplan/credit', {
     method: "POST",
     body: JSON.stringify({
-        "credit": credit_amount,
+        "credit_amount": credit_amount
     })
   })
   .then(response => response.json())
   .then(data => {
-    console.log("test");
+    getCredit();
   })
   .catch(error => console.log(error))
 })
@@ -52,4 +51,3 @@ creditRange.addEventListener('input', function() {
     const value = Number(creditRange.value).toLocaleString('de-DE');
     creditValue.innerText = value;
 });
-*/
--- HaptiPlan/templates/credit.php
+++ HaptiPlan/templates/credit.php
@@ -3,14 +3,16 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Credit</title>
+    <link rel="stylesheet" href="../styles/procurement.css">
 </head>
 
 <body>
     <?php include_once('header.php'); ?>
     <h1>Kredit</h1>
 
-    <form class="add_form" action='...../backend/HaptiPlan/credit' method="post">
+    <form class="add_form">
         <label for="credit">Kredit:</label>
         <input type="range" id="credit" name="credit" min="1000000" max="10000000" value="5000000" step="10000">
         <div id="creditValue">5.000.000</div>
@@ -22,7 +24,6 @@
             <div class="credit_amount"></div>
         </div>
     </template>
-
     <script src="../scripts/procurement_credit.js"></script>
 </body>
 
