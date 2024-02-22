415c2f6c6195206d38066f417a7802fdb8b30d00
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-02-22 11:59:10 +0100

    altered to fit player

--- HaptiPlan/templates/create_employee.php
+++ HaptiPlan/templates/create_employee.php
@@ -11,15 +11,17 @@
 <body>
     <?php include_once('header.php'); ?>
     <h1>Mitarbeiter:</h1>
-    <form class="add_form">
+    <!--form class="add_form">
         <div>
             <label for="employee_name">Name</label>
             <input type="text" name="employee_name" id="employee_name">
+        </div>
+        <div>
             <label for="employee_salary">Salary</label>
             <input type="text" name="employee_salary" id="employee_salary">
         </div>
         <button type="submit">create employee</button>
-    </form>
+    </form-->
     <ul class='dataList'></ul>
     <template class="employeeTemplate">
         <div class="employee_list">
@@ -30,6 +32,8 @@
             </form>
         </div>
     </template>
+    <div class="drop-zone" ondrop="drop(event)" ondragover="allowDrop(event)">
+    </div>
     <script src="../scripts/procurement_employee.js"></script>
 </body>
 
