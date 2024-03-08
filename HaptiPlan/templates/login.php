1adf0c2ebf67a164f040607e5bce7f13ac7ebccf
Nina Merkt <nina.merkt@student.jade-hs.de>
2024-03-08 13:11:06 +0100

    Login working, user must select role

--- HaptiPlan/templates/login.php
+++ HaptiPlan/templates/login.php
@@ -1,83 +1,49 @@
 <?php
 session_start();
 
-require_once ($_SERVER['DOCUMENT_ROOT']."/backend/HaptiPlan/include/database.php");
-
-// Create instance of Database class
-//TODO
-$db = Database::getInstance();
 
 // Check if user is already logged in, if yes, redirect them
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: add_machine.php");
+    switch ($_SESSION['user_role']) {
+        // Case admin
+        case 0:
+            exit;
+        // Case gamemaster
+        case 1:
+            header("location: create_machine.php");
+            exit;
+        // Case player
+        case 2:
+            header("location: add_machine.php");
+            exit;
+    }
     exit;
 }
 
-// Define variables and initialize with empty values
-$user_name = $user_password = "";
-$user_name_err = $user_password_err = "";
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if (isset($_POST['submit'])) {  
 
-    // Check if username is empty
-    if(empty(trim($_POST["user_name"]))){
-        $user_name_err = "Bitte gib deinen Username ein";
-    } else{
-        $user_name = trim($_POST["user_name"]);
-    }
+    // Store data in session variables
+    $_SESSION["loggedin"] = true;
+    $_SESSION["user_role"] = $_POST["user_role"];
 
-    // Check if password is empty
-    if(empty(trim($_POST["user_password"]))){
-        $user_password_err = "Bitte gib dein Password ein";
-    } else{
-        $user_password = trim($_POST["user_password"]);
-    }
-
-    // Validate credentials
-    if(empty($user_name_err) && empty($user_password_err)){
-        // Prepare a select statement
-        $sql = "SELECT user_id, user_name, user_password, user_role FROM users WHERE user_name = ?";
-
-        // Execute the query
-        $result = $db->query($sql, array($user_name));
-
-        if($result){
-            $row = $result[0];
-            $hashed_password = $row['user_password'];
-            $user_role = $row['user_role'];
-            if(password_verify($user_password, $hashed_password)){
-                session_start();
-                // Store data in session variables
-                $_SESSION["loggedin"] = true;
-                $_SESSION["user_id"] = $row['user_id'];
-                $_SESSION["user_name"] = $user_name;
-                $_SESSION["user_role"] = $user_role;
-
-                // Redirect user based on role
-                switch ($user_role) {
-                    // Case admin
-                    case 0:
-                        break;
-                    // Case gamemaster
-                    case 1:
-                        header("location: create_machine.php");
-                        break;
-                    // Case player
-                    case 2:
-                        header("location: add_machine.php");
-                        break;
-                }
-            } else{
-                // Display an error message if password is not valid
-                $user_password_err = "Das Password ist falsch";
-            }
-        } else{
-            // Display an error message if username doesn't exist
-            $user_name_err = "Es gibt keinen Account mit diesem Username";
-        }
+    // Redirect user based on role
+    switch ($_SESSION["user_role"]) {
+        // Case admin
+        case 0:
+            break;
+        // Case gamemaster
+        case 1:
+            header("location: create_machine.php");
+            break;
+        // Case player
+        case 2:
+            header("location: add_machine.php");
+            break;
     }
 }
+    
 ?>
 
 <!DOCTYPE html>
@@ -88,22 +54,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
     <?php include_once("header.php"); ?>
+
     <div class="wrapper">
         <h2>Login</h2>
         <form method="POST">
             <div>
                 <label>Username:</label>
-                <input type="text" name="user_name" value="<?php echo $user_name; ?>">
-                <span><?php echo $user_name_err; ?></span>
+                <input type="text" name="user_name" value="" required>
             </div>
             <div>
                 <label>Password:</label>
-                <input type="password" name="user_password" value="<?php echo $user_password; ?>">
-                <span><?php echo $user_password_err; ?></span>
+                <input type="password" name="user_password" value="" required>
             </div>
             <div>
-                <input type="submit" value="Login">
+                <label>Your role:</label>
+                <select name="user_role" id="user_role" required>
+                    <option value="" selected disabled hidden>- Choose here -</option>
+                    <option value="2">Player</option>
+                    <option value="1">Gamemaster</option>
+                    <option value="0">Admin</option>
+                </select>
             </div>
+            <button name="submit" type="submit">login</button>
         </form>
     </div>
 </body>
